271dda5e488b3f6f4890f11fbbe8ec581a5e29f6
therealblue24 <99767624+therealblue24@users.noreply.github.com>
2025-06-28 17:00:02 -0400

    ignore git

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ set -xe
 
 rm -f oldlight.zip
 
-zip -r oldlight.zip . -x build.sh -x demo.png -x .DS_Store -x README.md
+# thx https://askubuntu.com/questions/28476/how-do-i-zip-up-a-folder-but-exclude-the-git-subfolder
+zip -r oldlight.zip . -x build.sh -x demo.png -x '*.DS_Store*' -x README.md -x '*.git*'
